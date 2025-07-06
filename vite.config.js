cf18dcb67605a316e95217993d5f92b59cf13b75
dixitk941 <dixitk941@gmail.com>
2025-07-06 10:03:28 +0530

    feat: Configure Vite and Vercel settings for improved deployment and performance

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          markdown: ['react-markdown'],
+          helmet: ['react-helmet-async']
+        }
+      }
+    }
+  }
 })
